cf4d7ae21bb411ff7721d265991801ceebd0fcd6
midorigameee <xqbodp727@gmail.com>
2023-11-01 09:33:44 +0900

    表示文字を1文字ずつ要素分けしてアニメーションを追加
    （残課題）フェードインだが最初から表示されっぱなし

--- public/javascripts/TextMoving.js
+++ public/javascripts/TextMoving.js
@@ -2,27 +2,36 @@
 
 (window.onload = function() {
     //要素を取得 ..①
-    const sample = document.getElementsByClassName("text");
+    const word_elem_list = document.getElementsByClassName("sabu-word");
 
-    if(sample.length > 0){
-        console.log("sample : " + sample.length);
-        for(i=0; i<sample.length; i++){
-            console.log("sample : " + sample[i]);
+    if(word_elem_list.length <= 0){
+        return;
+    }
+
+    let whole_animation_times = 0;
+
+    // 要素の数だけfor文回す
+    for(let elem_num=0; elem_num<word_elem_list.length; elem_num++){
+        let elem = word_elem_list[elem_num];
+        let current_inner = elem.innerText;  // 現在の要素内の文字列
+        let new_inner = "";
+
+        // 要素内の文字数だけfor文回す
+        for(let char_num=0; char_num<current_inner.length; char_num++){
+            // 1文字ずつ<span>〜</span>で囲み、innerに追加
+            new_inner += '<span>' + current_inner[char_num] + '</span>'
         }
+
+        // 要素の中身を書き換える
+        elem.innerHTML = new_inner;
+
+        // 1文字ずつクラスを付与する
+        for(let child_num=0;child_num<elem.children.length;child_num++){
+            elem.children[child_num].classList.add("each");
+            elem.children[child_num].style.animationDelay = `${whole_animation_times*0.2}s`
+
+            whole_animation_times++;
+        }
+
     }
 })();
-
-/*
-//変換後のHTMLをここに配置 ..②
-let inner = "";
-
-//１文字ずつ処理をする ..③
-for(i=0;i<sample.innerText.length;i++) {
-
-    //<span>〜</span>で囲み、innerに追加 ..④
-    inner += `<span>${sample.innerText[i]}</span>`
-}
-
-//sampleのHTMLを変更 ..⑤
-sample.innerHTML = inner;
-*/

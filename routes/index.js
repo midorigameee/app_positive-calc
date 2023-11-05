e2f6ed6e1d40938e65c8d7a02c87bc3071746369
midorigameee <xqbodp727@gmail.com>
2023-11-05 09:12:43 +0900

    リファクタリング テンプレートで表示させる文字を全て変数化した

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const WORD_NUM = 8;
+const WORD_LIST = [
+  "脳の回転速すぎて火が出る！",
+  "アインシュタインの生まれ変わり！",
+  "脳みその皺がラビリンス！",
+  "ベガバンクもびっくり！",
+  "人間国宝！ルーブル美術館に展示したい！",
+  "脳みそ詰まりすぎ、密です！",
+  "脳みそは密でいいんだよ！",
+  "羂索「キッショ、なんで分かるんだよ」"
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var num1 = Math.floor(Math.random() * 100) + 1;
@@ -24,17 +36,28 @@ router.post('/result', function(req, res, next) {
   var num2 = Number(req.body.num2);
   var result = Number(req.body.result);
   var result_message = "Oh...";
+  var result_text = "伸びしろは無限大…！";
 
   var judgement = false;
   if((num1+num2) == result){
     judgement = true;
     result_message = "Congratulations!!"
+    result_text = "正解！天才です！！";
+  }
+
+  var result_words = [];
+  for(var i=0; i<WORD_NUM; i++){
+    var temp = "";
+    if(judgement) temp = WORD_LIST[i];
+
+    result_words.push(temp);
   }
 
   res.render('result', {
     title: 'Result',
     result_message: result_message,
-    judgement: judgement
+    result_text: result_text,
+    result_words: result_words
   });
 });
 
